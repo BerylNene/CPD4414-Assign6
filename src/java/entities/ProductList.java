4766be461eadf577a3575d45eacb4461bb949891
Beryl Bawo Nene <'nenebawoberyl@gmail.com'>
2015-04-04 20:46:20 -0400

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	products.sql
    	src/java/entities/ProductList.java
    	src/java/entities/Products.java
    	src/java/org/netbeans/rest/application/config/ApplicationConfig.java
    	src/java/services/ProductREST.java

--- src/java/entities/ProductList.java
+++ src/java/entities/ProductList.java
@@ -11,6 +11,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,6 +29,7 @@ public class ProductList {
     private List <Products> productList;
     
     public ProductList(){
+        productList=new ArrayList<>();
         try (Connection conn = getConnection()){
             String query = "SELECT * FROM products";
             PreparedStatement pstmt = conn.prepareStatement(query);
